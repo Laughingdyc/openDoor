db1d063d8d1e62b059906993a617715b82ce7a57
My Name Is DuYuChen <light_community@qq.com>
2023-03-20 01:28:13 +0800

    feat: openDoor

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import axios from 'axios'
+  const openDoor = () => {
+    axios.get("api/openDoor/open").then((res) => {
+      console.log(res)
+    })
+  }
+</script>
+
+<template>
+  <button @click="openDoor()">开门</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
